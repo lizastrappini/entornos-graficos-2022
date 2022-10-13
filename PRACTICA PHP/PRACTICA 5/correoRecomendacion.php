d968584073e7df68065ed7638ffe80be28938d24
lizastrappini <lizastrappini99@gmail.com>
2022-10-12 21:19:36 -0300

    borro pass

--- PRACTICA PHP/PRACTICA 5/correoRecomendacion.php
+++ PRACTICA PHP/PRACTICA 5/correoRecomendacion.php
@@ -50,7 +50,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                      	//Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'lizaotrascosas@gmail.com';                     //SMTP username
-    $mail->Password   = 'STRAPPINIliza10';                               //SMTP password
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
